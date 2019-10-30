17342b7be2dac70d4e50d61f9b33705e421b3c03
Alexis Li <alexis.li@oicr.on.ca>
2019-10-30 11:39:03 -0400

    Add ES mapping json files for workflow and task indices.

--- src/main/java/org/icgc_argo/workflow/relay/config/elastic/ElasticsearchStartupListener.java
+++ src/main/java/org/icgc_argo/workflow/relay/config/elastic/ElasticsearchStartupListener.java
@@ -1,5 +1,7 @@
 package org.icgc_argo.workflow.relay.config.elastic;
 
+import static org.icgc_argo.workflow.relay.util.StringUtilities.inputStreamToString;
+
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -8,11 +10,14 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 @Profile("index")
@@ -22,6 +27,12 @@ public class ElasticsearchStartupListener implements ApplicationListener<Context
 
   private final RestHighLevelClient client;
   private final ElasticsearchProperties properties;
+
+  @Value("classpath:run_log_mapping.json")
+  private Resource workflowIndexMapping;
+
+  @Value("classpath:task_log_mapping.json")
+  private Resource taskIndexMapping;
 
   @Autowired
   public ElasticsearchStartupListener(
@@ -45,11 +56,25 @@ public class ElasticsearchStartupListener implements ApplicationListener<Context
   private boolean ensureIndex(String indexName) {
     val request = new GetIndexRequest(indexName);
     if (!client.indices().exists(request, RequestOptions.DEFAULT)) {
+      val indexSource = loadIndexSourceAsString(indexName);
       val createRequest = new CreateIndexRequest(indexName);
       log.info("Creating index {}", indexName);
+      createRequest.source(indexSource, XContentType.JSON);
       val response = client.indices().create(createRequest, RequestOptions.DEFAULT);
       return response.isAcknowledged();
     }
     return true;
+  }
+
+  @SneakyThrows
+  private String loadIndexSourceAsString(String indexName) {
+    log.trace("in loadIndexSourceAsString: {}", indexName);
+    if (indexName.equals(properties.workflowIndex)) {
+      return inputStreamToString(workflowIndexMapping.getInputStream());
+    } else if (indexName.equals(properties.taskIndex)) {
+      return inputStreamToString(taskIndexMapping.getInputStream());
+    } else
+      throw new RuntimeException(
+          "Failed to load index source: index name must be workflow or task.");
   }
 }
