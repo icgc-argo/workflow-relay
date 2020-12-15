fcf55dad5aacf3c18eacc186d526edfce1700c4e
Alex <lepsalex@gmail.com>
2020-12-15 15:02:39 -0500

    underscores ftw

--- src/main/java/org/icgc_argo/workflow/relay/config/stream/GraphLogStream.java
+++ src/main/java/org/icgc_argo/workflow/relay/config/stream/GraphLogStream.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.messaging.SubscribableChannel;
 
 public interface GraphLogStream {
-    String INFODEBUG = "graphlog_infoDebug_index";
-    String WARNINGERROR = "graphlog_errorWarning_index";
+    String INFODEBUG = "graphlog_info_debug_index";
+    String WARNINGERROR = "graphlog_error_warning_index";
 
     @Input(INFODEBUG)
     SubscribableChannel debugInfoOutput();
