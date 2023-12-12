d7aaace34bd057a5ad71b2f83358a6d8a7069c74
Anders Richardsson <2107110+justincorrigible@users.noreply.github.com>
2023-12-12 13:19:07 -0500

    remove blankspace

--- src/main/java/org/icgc/argo/workflow/relay/service/IndexService.java
+++ src/main/java/org/icgc/argo/workflow/relay/service/IndexService.java
@@ -82,7 +82,6 @@ public class IndexService {
     this.taskIndex = elasticsearchProperties.getTaskIndex();
   }
 
-
   @StreamListener(IndexStream.WORKFLOW)
   public void indexWorkflow(JsonNode event) throws Exception{
     log.debug("workflow event: {}", event);
